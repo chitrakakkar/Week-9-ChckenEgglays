003533d58a8e2277982e2d2dfda8a7fc26b6f74b
chitrakakkar <India@123>
2016-03-25 12:52:47 -0500

    Changed the if statement in EggCounter
    Made changes in EggCounter

--- src/com/Chitra/EggCounter.java
+++ src/com/Chitra/EggCounter.java
@@ -7,42 +7,29 @@ import java.util.Observer;
 /**
  * Created by chitrakakkar on 3/25/16.
  */
-public class EggCounter implements Observer
-{
+public class EggCounter implements Observer {
     int Counter = 0;
+
     public void update(Observable chicken, Object o)
     {
+        //System.out.println(chicken + " has laid "+ chicken.countObservers() + "Eggs");
+
+
         String name = (((Chicken) chicken).name);
         {
             LinkedList<Chicken> ChickenList = new LinkedList<>();
             ChickenList.add(((Chicken) chicken));
-            for (Chicken c : ChickenList)
-            {
-                if (c.laidEgg() && (c.name.equals(name)))
-                {
-                    Counter++;
+            for (Chicken c : ChickenList) {
+                if (c.laidEgg() && (c.name.equals(name))) {
+                    this.Counter++;
                 }
 
                 System.out.println(chicken + " has laid " + Counter + " eggs");
 
             }
 
-//        if (((Chicken) chicken).laidEgg())
-//        {
-//            //if(((Chicken) chicken).name.equals(((Chicken) chicken).name))
-//
-//           this.Counter++;
-//
-//
-//        }
-//        System.out.println(chicken + " has laid " + this.Counter + " eggs");
-
-//        if (((Chicken) chicken).laidEgg())
-//        {
-//            Counter++;
-//            System.out.println(chicken + " has laid " + Counter + "Eggs");
-//        }
 
         }
     }
 }
+
